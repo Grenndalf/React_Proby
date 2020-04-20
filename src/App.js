384b7744f7830cd5bb17974a4ea2acbecf8ccc26
Bartosz Olczak <b.olczak4@wp.pl>
2020-04-20 22:34:18 +0200

    master again

--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class Spany extends Component {
-  // render() {
-  //   const { text1 } = this.props.display;
-  //   return <span className="yeah">{text1}</span>;
-  // }
+class ToDoItem extends Component {
+
+  render() {
+    const { text } = this.props.task
+    return (
+      <div className='yeah'>
+        <p> { text } </p>
+      </div>
+    );
+  }
 }
 
 class ToDoList extends Component {
   state = {
     tasks: this.props.tasks,
-    draft: "",
+    draft: '',
   };
-  upadateDraft = (event) => {
+  updateDraft = (event) => {
     this.setState({ draft: event.target.value });
   };
   addToDo = () => {
     const list = this.state.tasks;
-    list.push(this.state.draft);
-    this.setState({ tasks: list, draft: "" });
+    list.push({text: this.state.draft});
+    this.setState({ tasks: list, draft: '' });
   };
 
   render() {
+    const { title } = this.props;
+    const { draft } = this.state;
     return (
       <div>
-        <h1>{this.state.title}</h1>
+        <h1>{title}</h1>
         <div>
           <p>Naucz sie kuźwa propsów i state'ów</p>
         </div>
         <div className="flexer">
           {this.state.tasks.map((task) => (
-            <span className='yeah'>{task}</span>
+            <ToDoItem task = {task} />
           ))}
           <input
             className="myInput App"
             type="text"
             maxLength="10"
-            onChange={this.upadateDraft}
-            value={this.state.draft}
+            onChange={this.updateDraft}
+            value={draft}
           />
           <button className="App" onClick={this.addToDo}>
             Dawaj heheszki
@@ -50,11 +57,11 @@ class ToDoList extends Component {
 }
 
 class App extends Component {
- 
+  myTasks = [ {text: 'orzeł 1'} ,{text:'hmm'} ];
   render() {
     return (
       <div>
-        <ToDoList title="my Stuff!" tasks={[]} />
+        <ToDoList title="my Stuff!" tasks={this.myTasks} />
       </div>
     );
   }
