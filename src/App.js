0bc9006a2180ab8ae8bb0b49480ac1d56803247e
Bartosz Olczak <b.olczak4@wp.pl>
2020-04-25 11:23:04 +0200

    kolejne test - powoli do przodu

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { Component } from "react";
 import "./App.css";
 
 class ToDoItem extends Component {
+  counter = 0;
   state = {
-    done: false
+    done: false,
+    counter1: 0,
   };
-  doneToggle = () =>{
-    this.setState({done: !this.state.done})
-  }
+  doneToggle = () => {
+    this.setState({ done: !this.state.done });
+  };
+  componentDidMount = () => {
+    this.setState({ counter1: this.counter + 5 });
+  };
+
   render() {
     const { text } = this.props.task;
     return (
-      <div onClick={this.doneToggle} className={this.state.done?'done':'yeah'}>
+      <div
+        onClick={this.doneToggle}
+        className={this.state.done ? "done" : "yeah"}
+      >
         <p> {text} </p>
       </div>
     );
@@ -21,15 +30,20 @@ class ToDoItem extends Component {
 class ToDoList extends Component {
   state = {
     tasks: this.props.tasks,
-    draft: '',
+    draft: "",
   };
+
   updateDraft = (event) => {
     this.setState({ draft: event.target.value });
   };
+
   addToDo = () => {
+    const matcher = /^\s*$/;
     const list = this.state.tasks;
-    list.push({ text: this.state.draft });
-    this.setState({ tasks: list, draft: "" });
+    if (!this.state.draft.match(matcher)) {
+      list.push({ text: this.state.draft });
+      this.setState({ tasks: list, draft: "" });
+    }
   };
 
   render() {
@@ -63,10 +77,11 @@ class ToDoList extends Component {
 
 class App extends Component {
   myTasks = [{ text: "orzeł 1" }, { text: "hmm" }];
+  title = "jak to policzyć?";
   render() {
     return (
-      <div>
-        <ToDoList title="my Stuff!" tasks={this.myTasks} />
+      <div className="main-cont">
+        <ToDoList title={this.title} tasks={this.myTasks} />
       </div>
     );
   }
